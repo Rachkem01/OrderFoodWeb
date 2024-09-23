322d1a9543797360f69a94a95267ae9ade20f9d5
Rachkem01 <122974865+Rachkem01@users.noreply.github.com>
2024-09-23 14:09:10 +0100

    Update Context.jsx

--- FrontEnd/frontend/src/Context/Context.jsx
+++ FrontEnd/frontend/src/Context/Context.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 export const StoreContext = createContext(null)
  const StoreContextProvider = (props)=>{
     const [cartItems, setCartItems] = useState({})
-    const url = "https://rich-meals-backend.onrender.com"
+   const url = "https://rich-meals-backend.onrender.com"
     const [token,setToken] = useState("")
     const [food_list, setFoodList] = useState([])
 const addToCart = async(itemId)=>{
